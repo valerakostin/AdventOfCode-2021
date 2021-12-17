5d5356e81ba5d53a24e6cd437f791b18b9b87832
Valera Kostin <valera.kostin@gmail.com>
2021-12-17 18:25:40 +0100

    Day16 add part2 solution

--- src/Day16.kt
+++ src/Day16.kt
@@ -6,15 +6,28 @@ fun main() {
         val children = mutableListOf<Packet>()
         private var value: Long? = null
         private var limit = Integer.MAX_VALUE
+        var parent: Packet? = null
 
 
         fun addChild(p: Packet): Boolean {
             children.add(p)
+            p.parent = this
             return limit == children.size
+        }
+
+        fun getParentPacket(): Packet {
+            var p = parent
+            while (p != null && p.isFull())
+                p = p.parent
+            return p!!
         }
 
         fun setLimit(limit: Int) {
             this.limit = limit
+        }
+
+        fun isFull(): Boolean {
+            return children.size >= limit
         }
 
         fun setValue(value: Long) {
@@ -29,25 +42,19 @@ fun main() {
             return sum
         }
 
-//        fun getValue(): Long {
-//            return when (typeId) {
-//                4 -> return value!!
-//                0 -> return children.sumOf { it.getValue() }
-//                1 ->
-//                    children.map { it.getValue() }.reduce { acc, i -> acc * i }
-//                2 -> {
-//                    return children.minOf { it.getValue() }
-//                }
-//                3 -> {
-//                    return children.maxOf { it.getValue() }
-//                }
-//                5 -> if (children[0].getValue() > children[1].getValue()) 1 else 0
-//                6 -> if (children[0].getValue() < children[1].getValue()) 1 else 0
-//                7 -> if (children[0].getValue() == children[1].getValue()) 1 else 0
-//
-//                else -> {0}
-//            }
-//        }
+        fun getValue(): Long {
+            return when (typeId) {
+                4 -> value!!
+                0 -> children.sumOf { it.getValue() }
+                1 -> children.map { it.getValue() }.reduce { acc, i -> acc * i }
+                2 -> children.minOf { it.getValue() }
+                3 -> children.maxOf { it.getValue() }
+                5 -> if (children[0].getValue() > children[1].getValue()) 1 else 0
+                6 -> if (children[0].getValue() < children[1].getValue()) 1 else 0
+                7 -> if (children[0].getValue() == children[1].getValue()) 1 else 0
+                else -> 0
+            }
+        }
 
         override fun toString(): String {
             return "ver: $version type: $typeId  children: $children"
@@ -91,7 +98,8 @@ fun main() {
         val typeId = bs.substring(offset..offset + 2).toInt(2)
         offset += 3
 
-        val packet = Packet(version, typeId)
+        val parent = if (pPacket.isFull()) pPacket.getParentPacket() else pPacket
+        val child = Packet(version, typeId)
 
         if (typeId == literalCode) {
             val s = buildString {
@@ -104,8 +112,8 @@ fun main() {
                 }
             }
             val literal = s.toLong(2)
-            packet.setValue(literal)
-            pPacket.addChild(packet)
+            child.setValue(literal)
+            parent.addChild(child)
             parsePacket(pPacket, offset, bs)
         } else {
 
@@ -116,21 +124,17 @@ fun main() {
                 val str = bs.substring(offset, offset + length)
                 offset += length
 
-                parsePacket(packet, 0, str)
-                pPacket.addChild(packet)
-                parsePacket(pPacket, offset, bs)
+                parsePacket(child, 0, str)
+                parent.addChild(child)
+                parsePacket(parent, offset, bs)
 
             } else {
                 val lengthStr = bs.substring(offset..offset + 10)
                 val lm = lengthStr.toInt(2)
                 offset += 11
-                val r = pPacket.addChild(packet)
-                if (r)
-                    parsePacket(packet, offset, bs)
-                else {
-                    packet.setLimit(lm)
-                    parsePacket(packet, offset, bs)
-                }
+                child.setLimit(lm)
+                parent.addChild(child)
+                parsePacket(child, offset, bs)
             }
         }
     }
@@ -143,13 +147,13 @@ fun main() {
         return p.versionSum()
     }
 
-//    fun part2(input: String): Long {
-//        val pp = Packet(0, 0)
-//        val bs = convertToBinaryString(input)
-//        parsePacket(pp, 0, bs)
-//        val p = pp.children[0]
-//        return p.getValue()
-//    }
+    fun part2(input: String): Long {
+        val pp = Packet(0, 0)
+        val bs = convertToBinaryString(input)
+        parsePacket(pp, 0, bs)
+        val p = pp.children[0]
+        return p.getValue()
+    }
 
     check(part1("D2FE28") == 6)
     check(part1("38006F45291200") == 9)
@@ -159,16 +163,16 @@ fun main() {
     check(part1("C0015000016115A2E0802F182340") == 23)
     check(part1("A0016C880162017C3686B18A3D4780") == 31)
 
-//    check(part2("C200B40A82") == 3L)
-//    check(part2("04005AC33890") == 54L)
-//    check(part2("880086C3E88112") == 7L)
-//    check(part2("CE00C43D881120") == 9L)
-//    check(part2("D8005AC2A8F0") == 1L)
-//    check(part2("F600BC2D8F") == 0L)
-//    check(part2("9C005AC2F8F0") == 0L)
-//    check(part2("9C0141080250320F1802104A08") == 1L)
+    check(part2("C200B40A82") == 3L)
+    check(part2("04005AC33890") == 54L)
+    check(part2("880086C3E88112") == 7L)
+    check(part2("CE00C43D881120") == 9L)
+    check(part2("D8005AC2A8F0") == 1L)
+    check(part2("F600BC2D8F") == 0L)
+    check(part2("9C005AC2F8F0") == 0L)
+    check(part2("9C0141080250320F1802104A08") == 1L)
 
     val input = readInput("Day16")
     println(part1(input[0]))
-    //println(part2(input))
+    println(part2(input[0]))
 }
